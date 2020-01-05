90b54a8fa3d7838be8a94bfbdd93e735b171d763
ljm <1632444197@qq.com>
2020-01-05 19:08:32 +0800

    将保存的数据取出

--- ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"onCreate");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState!=null){
+            String tempData=savedInstanceState.getString("data_key");
+            Log.d(TAG,tempData);
+        }
         Button startNormalActivity = (Button)findViewById(R.id.start_normal_activity);
         Button startDialogActivity = (Button)findViewById(R.id.start_dialog_activity );
         startNormalActivity.setOnClickListener(new View.OnClickListener(){
