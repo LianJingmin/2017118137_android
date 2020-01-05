44a91c76419a4ac2d6b9ba51d5ec971b03bbdb4c
ljm <1632444197@qq.com>
2020-01-06 01:43:37 +0800

    编写CALL_PHONE权限代码

--- RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.runtimepermissiontest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button makeCall = (Button)findViewById(R.id.make_call);
+        makeCall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                   ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                           Manifest.permission.CALL_PHONE
+                   },1);
+               }else{
+                   call();
+               }
+            }
+        });
+    }
+    private void call(){
+        try{
+            Intent intent=new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch(SecurityException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void onRequestPermissionResult(int requestCode,String[] permissions,int[] grantResult){
+        switch (requestCode){
+            case 1:
+                if(grantResult.length>0&&grantResult[0]==PackageManager.PERMISSION_GRANTED){
+                    call();
+                }else{
+                    Toast.makeText(this,"You denied the permission",Toast.LENGTH_SHORT).show();
+                }
+                break;
+                default:
+        }
     }
 }
