cbcc6696b1d07a6fbfbc7e8bd23e3fa5f43c9bfc
ljm <1632444197@qq.com>
2020-01-05 22:26:10 +0800

    启动模式Standard

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -18,13 +18,14 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
         setContentView(R.layout.first_layout);
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-                startActivityForResult(intent,1);
+                Intent intent = new Intent(FirstActivity.this,FirstActivity.class);
+                startActivity(intent);
             }
         });
     }
