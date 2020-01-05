4434b22bf62bd7a0f9e01e3af69073b076dcccdb
ljm <1632444197@qq.com>
2020-01-06 00:15:38 +0800

    打印类名知晓界面对应的活动

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
