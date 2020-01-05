7d1e2e7b973fcb5f34bba6d1d547db944b39630d
ljm <1632444197@qq.com>
2020-01-05 15:52:50 +0800

    在活动中使用Toast

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,8 +1,12 @@
 package com.example.activitytest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this,"You clicked Button 1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
